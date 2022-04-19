35ecf6b6db87901b00b847c0144067149c90f7c4
Kang Wang <kangqiwang@outlook.com>
2022-04-19 17:13:05 +0100

    update code

--- Python/146. LRU Cache.py
+++ Python/146. LRU Cache.py
@@ -1,13 +1,26 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+
 
     def get(self, key: int) -> int:
-        pass
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key) 
+        self.dic[key] = v   # set key as the newest one
+        return v
 
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.dic:    
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1  
+            else:  # self.dic is full
+                self.dic.popitem(last=False) 
+        self.dic[key] = value
         
 
 
