201eafe6cfe4a834a2182dde512ee0be5af757e8
Kang Wang <kangqiwang@outlook.com>
2022-04-20 11:56:33 +0100

    update 200 number of islands

--- Python/randomTest.py
+++ Python/randomTest.py
@@ -1,10 +1,12 @@
 import collections
 
-dic =collections.OrderedDict()
-dic[1]=1
-dic[2]=2
-dic.pop(2)
-print(dic)
-dic[3]=3
-dic.popitem(last=False)
-print(dic)
+# dic =collections.OrderedDict()
+# dic[1]=1
+# dic[2]=2
+# dic.pop(2)
+# print(dic)
+# dic[3]=3
+# dic.popitem(last=False)
+# print(dic)
+
+    
