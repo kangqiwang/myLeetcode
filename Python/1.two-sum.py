83d565c8b92617bc12dc4134ace787e4dbba4d09
Kang Wang <kangqiwang@outlook.com>
2022-04-20 12:25:16 +0100

    update two sum

--- Python/1.two-sum.py
+++ Python/1.two-sum.py
@@ -3,6 +3,8 @@
 #
 # [1] Two Sum
 #
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -10,5 +12,16 @@ class Solution:
             if a in nums:
                 return [i, nums.index(a)+i+1]
 
-        
-
+    
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            print(map)   
+            if nums[i] not in map:
+                map[target - nums[i]] = i 
+                
+            else:
+                return [map[nums[i]], i]
+        return [-1, -1]
+   
+print(Solution().twoSum2([3,14,2,7],9))
--- Python/200. Number of Islands.py
+++ Python/200. Number of Islands.py
@@ -6,9 +6,7 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    print(i,j)
                     self.dfs(grid,i,j)
-                    
                     count  += 1
         return count
     
@@ -21,10 +19,11 @@ class Solution:
                 self.dfs(grid,r,c)
  
  
-    def dfs(self, grid, i,j):
-        grid[i][j] = 0
-        for dr,dc in (1,0),(-1,0),(0,-1),(0,1):
-            r= i+dr
-            c=dc+j
-            if 0<=r<len(grid) and 0<=c<len(grid[0]) and grid[r][c]=="1":
-                self.dfs(grid,r,c)
+    def dfs2(self,grid,i,j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
