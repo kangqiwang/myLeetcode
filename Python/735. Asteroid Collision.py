9cc235b49f993dd68c06432d1b56c7361159b1d7
Kang Wang <kangqiwang@outlook.com>
2022-04-26 15:34:45 +0100

    update asteroid collision

--- Python/735. Asteroid Collision.py
+++ Python/735. Asteroid Collision.py
@@ -3,10 +3,34 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack =[]
         for asteroid in asteroids:
-            while stack:
-                break
-            else:
+            if asteroid >0:
                 stack.append(asteroid)
+            else:
+                while stack and stack[-1] > 0 and stack[-1]+ asteroid <0:
+                    stack.pop()
+                if asteroid+stack[-1]>0:
+                    break
+                elif asteroid + stack[-1] <0:
+                    stack.pop()
+                else:
+                    stack.pop()
         return stack
-print(Solution().asteroidCollision([5,1,-5]))
+    
+    def asteroidCollision2(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            if a > 0:
+                stack.append(a)
+            else:
+                print(a)
+                while stack and stack[-1] > 0 and stack[-1] + a < 0:
+                    print("while loop")
+                    print(stack)
+                    stack.pop()
+                if not stack or stack[-1] < 0:
+                    stack.append(a)
+                elif stack[-1] + a == 0:
+                    stack.pop()
+        return stack
+print(Solution().asteroidCollision2([10,1,2,-5,-10]))
             
