81aa9182666e2b545eba5a1adaa207d018bfab49
Kang Wang <kangqiwang@outlook.com>
2022-05-03 15:28:12 +0100

    update heaptest

--- Python/heap/heapTest.py
+++ Python/heap/heapTest.py
@@ -1,12 +1,21 @@
 import heapq
-heapData = [10, 50, 60, 20, 70]
-heapq.heapify(heapData)
-print("Heap:")
-print(heapData)
-heapq.heappush(heapData, 40)
-print(heapData)
-heapq.heappush(heapData, 30)
-print(heapData)
+# heapData = [10, 50, 60, 20, 70]
+# heapq.heapify(heapData)
+# print("Heap:")
+# print(heapData)
+# heapq.heappush(heapData, 40)
+# print(heapData)
+# heapq.heappush(heapData, 30)
+# print(heapData)
 
-heapq.heappop(heapData)
-print(heapData)
+# heapq.heappop(heapData)
+lists= [[1,4,5],[1,3,4],[2,6]]
+h=[(1, 0), (1, 1), (2, 2)]
+while h:
+    val,i=heapq.heappop(h)
+    print("val = ",val)
+    print("i=",i)
+    print(lists[i])
+    
+    
+# print(heapData)
