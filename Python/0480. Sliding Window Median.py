24a293bcff05b2911c236476e921b3573f5344a2
Kang Wang <kangqiwang@outlook.com>
2022-06-18 23:59:47 +0100

    0480

---
+++ Python/0480. Sliding Window Median.py
@@ -0,0 +1,46 @@
+'''
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle values.
+
+For examples, if arr = [2,3,4], the median is 3.
+For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 = 2.5.
+You are given an integer array nums and an integer k. There is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the median array for each window in the original array. Answers within 10-5 of the actual value will be accepted.
+
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+Explanation: 
+Window position                Median
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
+ 1 [3  -1  -3] 5  3  6  7       -1
+ 1  3 [-1  -3  5] 3  6  7       -1
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6
+
+Example 2:
+
+Input: nums = [1,2,3,4,2,3,1,4,2], k = 3
+Output: [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+
+
+'''
+from typing import List
+from sortedcontainers import SortedList
+
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+       sortlst = SortedList()
+       res = []
+       for i in range(len(nums)):
+           lst.add(nums[i])            # O(logk)
+           if len(lst) > k:
+               lst.remove(nums[i-k])   # if we use heapq here, it takes O(k) here, but for sortedList, it takes O(logk)
+           if len(lst) == k:
+               median = lst[k//2] if k%2 == 1 else (lst[k//2-1] + lst[k//2]) / 2
+               res.append(median)
+       return res
