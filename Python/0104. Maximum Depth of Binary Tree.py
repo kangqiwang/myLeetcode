03bf70a79429ed2dbb207a0daad091449109270d
Kang Wang <kangqiwang@outlook.com>
2022-08-25 14:56:35 +0100

    update 0104

--- Python/0104. Maximum Depth of Binary Tree.py
+++ Python/0104. Maximum Depth of Binary Tree.py
@@ -38,6 +38,12 @@ class Solution:
             return 0
         return countDepth(root,depth = 0)
     
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        return max(l,r)+1
+
 
     
         
